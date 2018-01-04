95ea7e40e207ace9e913b2e8f770c0794a6d393c
David Tamrazov <david.v.tamrazov@gmail.com>
2018-01-04 12:29:23 -0500

    added signup with validation for unsuccessful signups

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "signup failure" do 
+
+    user_hash = {
+      name: "",
+      email: "user@invalid",
+      password: "foo",
+      password_confirmation: "bar"
+    }
+
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: user_hash }
+    end 
+    
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    
+  end 
+
+end
