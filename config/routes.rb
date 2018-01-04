0fefdacd7ab187c02d40ef4b9088edfc1b6c3315
David Tamrazov <david.v.tamrazov@gmail.com>
2018-01-04 14:22:54 -0500

    Added sessions controller, view, and erb to render

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  
   root 'static_pages#home'
+  
+  get 'users/new'
 
   get 'static_pages/home'
 
@@ -14,6 +15,14 @@ Rails.application.routes.draw do
   get '/signup',  to: 'users#new'
 
   post '/signup', to: 'users#create'
+
+  get '/login',  to:  'sessions#new'
+
+  get '/login',   to: 'sessions#new'
+
+  post '/login',   to: 'sessions#create'
+
+  delete '/logout',   to: 'sessions#destroy'
   
   resources :users
 
